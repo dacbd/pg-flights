f621d37b6af8f24a5bbdb76aee48a5eaf64bf9f6
Daniel Barnes <dabarnes2b@gmail.com>
2014-05-02 15:26:17 -0700

    more flights

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
 var saveFile = require('./save');
 
-var filearray = fs.readdirSync('./kml/');
+var filearray = fs.readdirSync('./new/');
 console.log(filearray);
 
 for (var i = 0, ln = filearray.length; i < ln; i++) {
     var filename = filearray[i];
-    var filestat = fs.statSync('kml/' + filename);
+    var filestat = fs.statSync('new/' + filename);
     console.log('trying ' + filename);
     if (filestat.isFile()) {
         var split = filename.split('.');
         if (split[split.length - 1] === 'kml') {
-            saveFile( 'kml/' + filename);
+            saveFile( 'new/' + filename);
             console.log('done with ' + filename);
         }
     }
